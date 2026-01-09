0cf30e06cf0ddbc2e640412157a80b62fd440f90
BAMTEFA Olorunshogo Moses <shownzy001@gmail.com>
2026-01-09 18:42:23 +0100

    2hrs

--- Node/app.js
+++ Node/app.js
@@ -1,2 +1,9 @@
 // Manual approach (create package.json in the root, create properties etc)
-//  
+//  npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+const _ = require('lodash');
+
+const items = [1, 2, [3, [4]]];
+console.log(newItems);
+// const newItems = _.flattenedDeep(items);
